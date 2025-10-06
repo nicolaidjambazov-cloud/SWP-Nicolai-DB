e38bf6235884cf4addac5c0e4c058478b7844745
Nicolai Djambazov-Bichler <deine.email@example.com>
2025-10-06 05:14:42 -0700

    ljnfushfhyxiuc

---
+++ 2025-09-29_bruch/Auftrag_06_10_2025/Random_Brüche.ts
@@ -0,0 +1,18 @@
+const zeahler=Math.ceil(Math.random()*100);
+const nenner=Math.ceil(Math.random()*100)
+let zeahler2=Math.ceil(Math.random()*100);
+let ausgangszähler;
+if(zeahler>zeahler2)
+{
+    ausgangszähler=zeahler-zeahler2
+}
+
+
+
+
+
+
+
+
+
+
