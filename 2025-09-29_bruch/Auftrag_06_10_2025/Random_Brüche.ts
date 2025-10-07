cc33cdb337565ed814c3512b296ee2d3a63c54e1
Nicolai Djambazov-Bichler <deine.email@example.com>
2025-10-07 09:09:30 -0700

    j,j,jasy

--- 2025-09-29_bruch/Auftrag_06_10_2025/Random_Brüche.ts
+++ 2025-09-29_bruch/Auftrag_06_10_2025/Random_Brüche.ts
@@ -1,11 +1,29 @@
+import{Bruch} from "./bruch.ts";
+import { assertEquals } from "@std/assert";
+
+let ausgangszähler;
+let i;
+let e;
+
+for(e=0;e<15;e++)
+{
+while(i==0)
+{
+const ganz=Math.ceil(Math.random()*10);
 const zeahler=Math.ceil(Math.random()*100);
 const nenner=Math.ceil(Math.random()*100)
-let zeahler2=Math.ceil(Math.random()*100);
-let ausgangszähler;
-if(zeahler>zeahler2)
-{
-    ausgangszähler=zeahler-zeahler2
+const zeahler2=Math.ceil(Math.random()*100);
+    if(zeahler>zeahler2)
+    {
+        ausgangszähler=zeahler-zeahler2;
+        console.log("Anfangsbruch: "+ganz+" "+zeahler+"/"+nenner);
+        console.log("Subtrahierter Bruch: "+ganz+" "+ausgangszähler+"/"+nenner);
+        i=1;
+    }
 }
+
+};
+
 
 
 
