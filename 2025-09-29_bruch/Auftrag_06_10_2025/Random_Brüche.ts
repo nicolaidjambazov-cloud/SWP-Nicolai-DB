fad2a3f956a0b5f0f424f597b096802b545b4eaf
Nicolai Djambazov-Bichler <deine.email@example.com>
2025-10-09 02:08:06 -0700

    snaflnafjnsalfd

--- 2025-09-29_bruch/Auftrag_06_10_2025/Random_Brüche.ts
+++ 2025-09-29_bruch/Auftrag_06_10_2025/Random_Brüche.ts
@@ -1,27 +1,28 @@
 import{Bruch} from "./bruch.ts";
 import { assertEquals } from "@std/assert";
 
-let ausgangszähler;
-let i;
-let e;
+let e=0
 
-for(e=0;e<15;e++)
+while(e<15)
 {
-while(i==0)
-{
-const ganz=Math.ceil(Math.random()*10);
-const zeahler=Math.ceil(Math.random()*100);
-const nenner=Math.ceil(Math.random()*100)
-const zeahler2=Math.ceil(Math.random()*100);
-    if(zeahler>zeahler2)
-    {
-        ausgangszähler=zeahler-zeahler2;
-        console.log("Anfangsbruch: "+ganz+" "+zeahler+"/"+nenner);
-        console.log("Subtrahierter Bruch: "+ganz+" "+ausgangszähler+"/"+nenner);
-        i=1;
-    }
-}
+    const Zähler_ergebnis=Math.ceil(Math.random()*100);
+    const zeahler=Math.ceil(Math.random()*100);
+    const nenner=Math.ceil(Math.random()*100)
+    const erweiterer=Math.ceil(Math.random()*10);
+    const zeahler2=Zähler_ergebnis-zeahler;
+    
+    const erweiterterNenner=nenner*erweiterer;
+    const erweiterterZähler1=zeahler*erweiterer;
+    const erweiterterZähler2=zeahler2*erweiterer;
 
+    const b1 = new Bruch(0,erweiterterZähler1,erweiterterNenner);
+    const b2= new Bruch(0,erweiterterZähler2,erweiterterNenner);
+
+    Deno.test(`Bruch Addition Random Test`, () => {
+        assertEquals(b1.addiere(b2).toString(), Zähler_ergebnis.toString());
+    });    
+    e++
+   
 };
 
 
