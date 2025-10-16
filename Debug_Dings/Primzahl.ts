183a29d01c88ec22cb3165bb7bbf2df35f06b2f5
Nicolai Djambazov-Bichler <deine.email@example.com>
2025-10-16 06:18:23 -0700

    grg

--- Debug_Dings/Primzahl.ts
+++ Debug_Dings/Primzahl.ts
@@ -1,20 +1,17 @@
 function primzahl(n: number): [boolean, string] {
-    if (n <= 1) return [false, n+ " ist zu klein"];
-    const w=Math.sqrt(n);
-    for(let i=2; i< w ; i++)
-    {
-        if(n%i==0) // also teilbar
-        {
-            return[false, n+ " Keine Primzahl"];
+    if (n <= 1) return [false, n + " ist zu klein"];
+    const w = Math.sqrt(n);
+    for (let i = 2; i <= w; i++) {
+        if (n % i == 0) { // also teilbar
+            return [
+                false,
+                `Die Zahl ${n} ist keine Primzahl, weil sie durch ${i} teilbar ist`,
+            ];
         }
-        else
-        {
-            return[true, n+ " Ist eine Primzahl"]
-        },
-    }   
+    }
+    return [true, `Die Zahl ${n} ist eine Primzahl`];
 }
 
-for(let i=1;i<20;i++)
-{
-    console.log(primzahl(i))
+for (let i = 10000; i < 10100; i++) {
+    console.log(primzahl(i));
 }
